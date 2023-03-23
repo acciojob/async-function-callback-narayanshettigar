eb2bab6b7077f5f32ba4f586473331cefaf4aeae
narayanshettigar <117894336+narayanshettigar@users.noreply.github.com>
2023-03-24 00:40:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+//const url = "https://jsonplaceholder.typicode.com/posts/1";
 
 //your JS code here. If required.
+// define async function with callback parameter
+async function fetchData(callback) {
+	try {
+		const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+		const data = await response.json();
+		callback(data.title);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+// add click event listener to button
+document.getElementById('btn').addEventListener('click', async () => {
+	// call fetchData with callback function to display title in output div
+	fetchData((title) => {
+		document.getElementById('output').textContent = title;
+	});
+});
